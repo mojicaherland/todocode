b4f0be25c97be6de2853f99ee97dd7c08768e326
Herland Mojica <mojicacardozoh@gmail.com>
2024-01-20 16:46:46 -0400

    second commit

--- src/holamundo/Matrices.java
+++ src/holamundo/Matrices.java
@@ -4,23 +4,56 @@ import java.util.Scanner;
 
 public class Matrices {
     public static void main(String[] args) {
-        int matriz[][] = new int[3][3];
+        Matrices n = new Matrices();
+//        int matriz[][] = new int[3][3];
+//
+//        Scanner datoUser = new Scanner(System.in);
+//        //guardando los datos del user
+//        for (int fila = 0; fila < 2; fila++) {
+//            for (int columna = 0; columna < 2; columna++) {
+//                System.out.println("ingrese el dato para el indice : " + fila + columna);
+//                matriz[fila][columna] = datoUser.nextInt(); // guardando el dato del user
+//            }
+//        }
+//
+//        //mostrando los datos del user
+//        for (int fila = 0; fila < 2; fila++) {
+//            for (int columna = 0; columna < 2; columna++) {
+//                System.out.println("indice : " + fila + columna);
+//                System.out.println("tengo guardado un : " + matriz[fila][columna]);
+//            }
+//        }
+        int matriz[][] = new int[4][4];
+        //System.out.println(n.retornaNumeroRandom(100, 1));
+        //cargarDatosMatriz(matriz);
+        cargarDatosMatrizRandom(matriz);
+    }
 
+    //Funcion que retorna un numero random
+    public static int retornaNumeroRandom(int max, int min) {
+        return (int) (Math.random() * (max - min + 1) + min);
+    }
+
+    //CARGANDO DATOS Y PONIENDO DATOS A LA MATRIZ MANUALMENTE
+    public static void cargarDatosMatriz(int matriz[][]) {
         Scanner datoUser = new Scanner(System.in);
-        //guardando los datos del user
-        for (int fila = 0; fila < 2; fila++) {
-            for (int columna = 0; columna < 2; columna++) {
-                System.out.println("ingrese el dato para el indice : " + fila + columna);
-                matriz[fila][columna] = datoUser.nextInt(); // guardando el dato del user
-            }
-        }
-
-        //mostrando los datos del user
-        for (int fila = 0; fila < 2; fila++) {
-            for (int columna = 0; columna < 2; columna++) {
-                System.out.println("indice : " + fila + columna);
-                System.out.println("tengo guardado un : " + matriz[fila][columna]);
+        for (int fila = 0; fila < 4; fila++) {
+            for (int columna = 0; columna < 4; columna++) {
+                System.out.println("ingrese dato para la pos : " + fila + " " + +columna);
+                matriz[fila][columna] = datoUser.nextInt();
+                System.out.println("indice : " + fila + columna + " tengo guardado un : " + matriz[fila][columna]);
             }
         }
     }
+
+    // CARGANDO DATOS , PONIENDO DATOS Y MOSTRANDO DATOS A LA MATRIZ RANDOMICAMENTE
+    public static void cargarDatosMatrizRandom(int matriz[][]) {
+        for (int fila = 0; fila < 4; fila++) {
+            for (int columna = 0; columna < 4; columna++) {
+                matriz[fila][columna] = retornaNumeroRandom(100,1);
+                System.out.println("Pos : " + fila + " " + columna + "; Dato: " + matriz[fila][columna]);
+            }
+        }
+    }
+
 }
--- src/holamundo/Vectores.java
+++ src/holamundo/Vectores.java
@@ -4,18 +4,39 @@ import java.util.Scanner;
 
 public class Vectores {
     public static void main(String[] args) {
-        Scanner datoUser = new Scanner(System.in);
-        int vector[] = new int[4];
-        //cargando datos
-        for (int i = 0 ; i<vector.length;i++){
-            System.out.println("ingrese el valor para el indice : " + i);
-            vector[i] = datoUser.nextInt(); // en la posicion del indice ingresa el dato del user
-        }
+//        Scanner datoUser = new Scanner(System.in);
+//        int vector[] = new int[4];
+//        //cargando datos
+//        for (int i = 0 ; i<vector.length;i++){
+//            System.out.println("ingrese el valor para el indice : " + i);
+//            vector[i] = datoUser.nextInt(); // en la posicion del indice ingresa el dato del user
+//        }
+//
+//        //recorrido
+//        for (int i = 0; i < vector.length; i++) {
+//            System.out.println("estoy en el indice : " + i);
+//            System.out.println("tengo guardado un " + vector[i]); // muestra el dato que esta en la pos del vector
+//        }
 
-        //recorrido
+        int vector[] = new int[4];
+        cargarVectorRandom(vector);
+    }
+
+    //CARGA DATOS MANUALMENTE AL VECTOR Y LOS MUESTRA
+    public static void cargarVectorManual(int vector[]) {
+        Scanner datoUser = new Scanner(System.in);
         for (int i = 0; i < vector.length; i++) {
-            System.out.println("estoy en el indice : " + i);
-            System.out.println("tengo guardado un " + vector[i]); // muestra el dato que esta en la pos del vector
+            System.out.println("Ingrese dato para la pos : " + i);
+            vector[i] = datoUser.nextInt();
+            System.out.println("Pos : " + i + "; Dato : " + vector[i]);
+        }
+    }
+
+    // CARGA DATOS RANDOMICAMENTE AL VECTOR Y LOS MUESTRA
+    public static void cargarVectorRandom(int vector[]) {
+        for (int i = 0; i < vector.length; i++) {
+            vector[i] = Matrices.retornaNumeroRandom(100, 1);
+            System.out.println("Pos : " + i + "; Dato: " + vector[i]);
         }
     }
 }
